41ba3d517ad50123756c36e441d277e0cac02cc2
swamini10 <swaminibhandare10@gmail.com>
2026-02-03 17:35:45 +0530

    pattern

---
+++ Tasks by sir/pattern.java
@@ -0,0 +1,58 @@
+/*
+public class pattern {
+    public static void main(String args[]){
+        for(int line = 1; line <= 4 ; line++){
+            for(int ast = 1; ast <= line; ast++){
+                System.out.print("*");
+            }
+            System.out.println(""); 
+       }
+	}
+}
+
+o/p:-
+*
+**
+***
+****
+
+*/
+
+/*
+public class pattern {
+	public static void main (String args[]){
+		for(int line =4 ;line>=1; line--){
+			for(int num =1 ;num<=line; num++){
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+	}
+}
+
+o/p:-
+****
+***
+**
+*
+
+*/
+/*
+public class pattern{
+	public static void main(String args[]){
+		for(int i=1;i<=4;i++){
+			for(int j=2;j<=i+1;j++){
+				System.out.print(j);
+			}
+			System.out.println("");
+		}
+	}
+}
+
+o/p:-
+2
+23
+234
+2345
+
+*/
