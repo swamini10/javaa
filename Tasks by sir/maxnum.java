0a51ebd75d39dc444a17e7971ffdc6f6f2b61962
swamini <swaminibhandare10@gmail.com>
2026-02-09 00:29:06 +0530

    max number from array

---
+++ Tasks by sir/maxnum.java
@@ -0,0 +1,32 @@
+// find and print max number from array
+
+
+import java.util.Scanner;
+	public class maxnum
+{
+		public static void main(String args [])
+	  {
+		Scanner sc = new Scanner(System.in);
+        System.out.print("Enter array size : ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.print("Enter " + n + " values:");
+		int  i;
+        for ( i = 0; i < n; i++) 
+		{
+            arr[i] = sc.nextInt(); 
+			
+			System.out.print(arr[i]+" ");
+			
+		} System.out.println();
+		int max = arr[0];
+		  for (int  j = 0; j < n; j++) 
+		{ 
+	    if(arr[j]>max){
+		max = arr[j];
+	     }
+		}
+		System.out.print("Max element of the given array is : "+max);
+    }
+}
+
