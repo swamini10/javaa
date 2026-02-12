003a52dc90066f8730ff88ecd24871b61722b3f7
swamini <swaminibhandare10@gmail.com>
2026-02-12 20:10:45 +0530

    full diamond star pattern

---
+++ Assignment7/DiamondPattern.java
@@ -0,0 +1,29 @@
+//Print a full diamond star pattern.
+
+public class DiamondPattern {
+	public static void main (String args[]){
+	int n=5;
+	    for(int line=1;line<=n; line++)
+		{
+			for (int space= 1; space <= n - line; space++) 
+			{
+                System.out.print(" ");
+            }
+			for(int star=1;star<=line; star++)
+			{
+				System.out.print(" *");
+		    }
+		   System.out.println();
+		}
+		for(int line=n-1;line>=1; line--){
+			 for (int space= 1; space <= n - line; space++) 
+			 {
+                System.out.print(" ");
+            }
+			for(int star=1;star<=line; star++){
+				System.out.print(" *");
+		   }
+		   System.out.println();
+		}
+    }
+}
