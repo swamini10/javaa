58f52fa1c7e7035232f339250a62366740d70942
swamini <swaminibhandare10@gmail.com>
2026-02-12 18:58:52 +0530

    right-angled triangle

--- Assignment7/righttriangle.java
+++ Assignment7/righttriangle.java
@@ -1,23 +1,4 @@
 //Print a right-angled triangle star pattern.
-/*
-public class righttriangle{
-	public static void main(String args[]){
-		int n=5;
-		for(int i=1;i<=n; i++){
-			for(int j=i;j<=n; j++){
-				System.out.print(" *");
-		   }
-		   System.out.println();
-		}
-	}
-}
-
- * * * * *
- * * * *
- * * *
- * *
- *
-*/
 
 public class righttriangle{
 	public static void main(String args[]){
