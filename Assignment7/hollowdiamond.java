8b023617d900f8452f7292543071d433f9c1bf0c
swamini <swaminibhandare10@gmail.com>
2026-02-12 22:39:43 +0530

    hollow diamond

---
+++ Assignment7/hollowdiamond.java
@@ -0,0 +1,47 @@
+//Print a hollow diamond.
+
+
+public class hollowdiamond {
+	
+	public static void main (String args[]){
+	int n=5;
+	    for(int line=1;line<=n; line++)
+		{
+			for (int space= 1; space <= n - line; space++) 
+			{
+                System.out.print(" ");
+            }
+			for(int star=1;star<=line; star++)
+			{
+				if(star==1||line==1||star==line)
+				System.out.print(" *");
+			else
+				System.out.print("  ");
+		    }
+		   System.out.println();
+		}
+		for(int line=n-1;line>=1; line--){
+			 for (int space= 1; space <= n - line; space++) 
+			 {
+                System.out.print(" ");
+            }
+			for(int star=1;star<=line; star++){
+				if(star==1||line==1||star==line)
+				System.out.print(" *");
+			else
+				System.out.print("  ");
+		   }
+		   System.out.println();
+		}
+    }
+}
+
+     *
+    * *
+   *   *
+  *     *
+ *       *
+  *     *
+   *   *
+    * *
+     *
