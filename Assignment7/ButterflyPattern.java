d182a34e1acff99f01bf0679c36cb1a78f4c2b52
swamini <swaminibhandare10@gmail.com>
2026-02-12 23:51:12 +0530

    butterfly star pattern

--- Assignment7/ButterflyPattern.java
+++ Assignment7/ButterflyPattern.java
@@ -8,15 +8,15 @@ public class ButterflyPattern {
         for (int i = 1; i <= n; i++) {
 
             for (int j = 1; j <= i; j++) {
-                System.out.print("*");
+                System.out.print(" *");
             }
 
             for (int j = 1; j <= 2 * (n - i); j++) {
-                System.out.print(" ");
+                System.out.print("  ");
             }
 
             for (int j = 1; j <= i; j++) {
-                System.out.print("*");
+                System.out.print(" *");
             }
 
             System.out.println();
@@ -25,28 +25,29 @@ public class ButterflyPattern {
         for (int i = n; i >= 1; i--) {
 
             for (int j = 1; j <= i; j++) {
-                System.out.print("*");
+                System.out.print(" *");
             }
 
             for (int j = 1; j <= 2 * (n - i); j++) {
-                System.out.print(" ");
+                System.out.print("  ");
             }
             for (int j = 1; j <= i; j++) {
-                System.out.print("*");
+                System.out.print(" *");
             }
 
             System.out.println();
         }
     }
 }
-
-*        *
-**      **
-***    ***
-****  ****
-**********
-**********
-****  ****
-***    ***
-**      **
-*        *
+/*
+ *                 *
+ * *             * *
+ * * *         * * *
+ * * * *     * * * *
+ * * * * * * * * * *
+ * * * * * * * * * *
+ * * * *     * * * *
+ * * *         * * *
+ * *             * *
+ *                 *
+*/
