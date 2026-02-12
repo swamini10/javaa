3c5ca4a25446010228bd9fa90236cbc7cc4b1189
swamini <swaminibhandare10@gmail.com>
2026-02-12 20:00:16 +0530

    inverted pyramid pattern

---
+++ Assignment7/invertpyramid.java
@@ -0,0 +1,25 @@
+//Print an inverted pyramid pattern.
+
+public class invertpyramid{
+	public static void main (String args[]){
+		int n=5;
+		for(int line=n;line>=1; line--){
+			 for (int space= 1; space <= n - line; space++) 
+			 {
+                System.out.print(" ");
+            }
+			for(int star=1;star<=line; star++){
+				System.out.print(" *");
+		   }
+		   System.out.println();
+		}
+	  
+	}
+}
+/*
+ * * * * *
+  * * * *
+   * * *
+    * *
+     *
+*/
