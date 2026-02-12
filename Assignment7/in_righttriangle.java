92baf0392d5adeb68f91a9a4fb22cc3e986770d3
swamini <swaminibhandare10@gmail.com>
2026-02-12 18:59:16 +0530

    inverted  right-angled triangle

---
+++ Assignment7/in_righttriangle.java
@@ -0,0 +1,19 @@
+//Print an inverted right-angled triangle.
+
+public class in_righttriangle{
+	public static void main(String args[]){
+		int n=5;
+		for(int i=1;i<=n; i++){
+			for(int j=i;j<=n; j++){
+				System.out.print(" *");
+		   }
+		   System.out.println();
+		}
+	}
+}
+
+ * * * * *
+ * * * *
+ * * *
+ * *
+ *
