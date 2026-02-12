019f82eeb78a3f9b15c1ae838f848060356c1265
swamini <swaminibhandare10@gmail.com>
2026-02-13 00:50:26 +0530

    plus (+) pattern using stars

---
+++ Assignment7/pluspattern.java
@@ -0,0 +1,24 @@
+//Print a plus (+) pattern using stars.
+
+public class pluspattern {
+	public static void main(String args[]){
+		int n=5;
+		int mid=n/2;
+		for(int i=0;i<n; i++){
+			for(int j=0;j<n; j++){
+			if(i==mid||j==mid)
+				System.out.print(" *");
+		   else
+		   System.out.print("  ");
+		   }
+		   System.out.println();
+		}
+	}
+}
+
+
+     *
+     *
+ * * * * *
+     *
+     *
