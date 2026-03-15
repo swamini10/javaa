33dcc4f4d87965c4fabd2becefc4bc7f48acb6cd
swamini <swaminibhandare10@gmail.com>
2026-03-15 19:10:50 +0530

    default constructor

--- Assignment9/student.java
+++ Assignment9/student.java
@@ -1,8 +1,8 @@
-// Create a class Student with a default constructor that prints "Student Object Created".
+//Create a class Student with a default constructor that prints "Student Object Created".
 
 public class student { 
 	public student(){
-		System.out.println("student default constructor ");
+		System.out.println("Student Object Created ");
 	}
 	public static void main(String args[]){
 		student stu = new student();
