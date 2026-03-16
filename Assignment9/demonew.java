2c399b32d250d6ce2cc9ab0fa116c0e38f91f3c9
swamini <swaminibhandare10@gmail.com>
2026-03-16 13:27:17 +0530

    class with no constructor and check what happens when the object is created.

---
+++ Assignment9/demonew.java
@@ -0,0 +1,10 @@
+//Create a class with no constructor and check what happens when the object is created
+
+public class demonew{
+	public static void main(String args[]){
+		demonew d =new demonew();
+	}
+}
+
+
+If a class does not have a constructor, the compiler automatically creates a default constructor
