65fe2554b7cc91345844a42f712686cd9b8168bc
swamini <swaminibhandare10@gmail.com>
2026-03-16 13:12:08 +0530

    Rectangle with constructor that calculates are

---
+++ Assignment9/Rectanglearae.java
@@ -0,0 +1,10 @@
+//Create a class Rectangle with constructor that calculates area.
+public class  Rectanglearae{
+		public Rectanglearae(int l,int b){
+		int area =l*b;
+		        System.out.println("Rectangle area: " + area);
+		}
+		public static void main(String args[]){
+			Rectanglearae rec = new Rectanglearae(10,30	);
+		}
+}
