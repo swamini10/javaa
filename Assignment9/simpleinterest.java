6cf3f4739566ba9d1986179493d7f7cd17a0c193
swamini <swaminibhandare10@gmail.com>
2026-03-19 00:26:16 +0530

    calculates simple interest

--- Assignment9/simpleinterest.java
+++ Assignment9/simpleinterest.java
@@ -1,4 +1,28 @@
 //Create a program that calculates simple interest using constructor
-public class simpleinterest{
-       simpleinterest
+import java.util.Scanner;
+
+public class simpleinterest {
+    double p, r, t, si;
+
+    simpleinterest() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Principal: ");
+        p = sc.nextDouble();
+
+        System.out.print("Enter Rate: ");
+        r = sc.nextDouble();
+
+        System.out.print("Enter Time: ");
+        t = sc.nextDouble();
+
+        si = (p * r * t) / 100;
+
+        System.out.println("Simple Interest = " + si);
+    }
+
+    public static void main(String[] args) {
+        simpleinterest obj = new simpleinterest(); // constructor called
+        
+    }
 }
