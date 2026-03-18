cddee4ed253ef7b5ec88c0e70ce45acba87f43eb
swamini <swaminibhandare10@gmail.com>
2026-03-19 00:02:02 +0530

    checks prime number

---
+++ Assignment9/primenum.java
@@ -0,0 +1,25 @@
+//Write a constructor program that checks prime number
+
+public class primenum{
+public primenum(int n){
+	 boolean isPrime = true;
+
+        if(n <= 1) {
+            isPrime = false;
+        } else {
+            for(int i = 2; i <= n / 2; i++) {
+                if(n % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+		if(isPrime)
+            System.out.println(n + " is a Prime Number");
+        else
+            System.out.println(n + " is NOT a Prime Number");
+}
+	public static void main(String args[]){
+	primenum pri = new primenum(8);
+	}
+}
