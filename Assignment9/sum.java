c44bbd9d630ed884bba799140f6c082c899abc08
swamini <swaminibhandare10@gmail.com>
2026-03-18 23:35:04 +0530

    calculates sum of two numbers.

---
+++ Assignment9/sum.java
@@ -0,0 +1,10 @@
+//Create a constructor that calculates sum of two numbers.
+public class sum{
+public sum(int n1, int n2){
+	int add = n1+n2;
+	System.out.println(add);
+}
+	public static void main(String args[]){
+	sum s = new sum(20,10);
+	}
+}
