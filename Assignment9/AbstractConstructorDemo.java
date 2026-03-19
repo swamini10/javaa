782855ed1892470eb232234ba670723884bb6106
swamini <swaminibhandare10@gmail.com>
2026-03-19 11:39:53 +0530

    constructor in abstract class.

---
+++ Assignment9/AbstractConstructorDemo.java
@@ -0,0 +1,28 @@
+//Write a program demonstrating constructor in abstract class.
+
+abstract class Animal {
+
+    Animal() {
+        System.out.println("Animal constructor called");
+    }
+
+    abstract void sound();
+}
+
+class Dog extends Animal {
+
+    Dog() {
+        System.out.println("Dog constructor called");
+    }
+
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+public class AbstractConstructorDemo {
+    public static void main(String args[]) {
+        Dog d = new Dog();
+        d.sound();
+    }
+}
