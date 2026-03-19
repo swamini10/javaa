257cf90eee9eaa57dfbe09fd077616428513e68d
swamini <swaminibhandare10@gmail.com>
2026-03-19 11:41:42 +0530

    (Singleton idea).

---
+++ Assignment9/SingletonDemo.java
@@ -0,0 +1,36 @@
+//Create a class that prevents object creation using private constructor (Singleton idea).
+
+
+class Singleton {
+
+    // Step 1: Create a static instance
+    private static Singleton obj = new Singleton();
+
+    // Step 2: Private constructor (prevents object creation from outside)
+    private Singleton() {
+        System.out.println("Singleton object created");
+    }
+
+    // Step 3: Public method to provide access
+    public static Singleton getInstance() {
+        return obj;
+    }
+}
+
+// Main class
+public class SingletonDemo {
+    public static void main(String args[]) {
+
+        // Cannot do this:
+        // Singleton s = new Singleton(); ❌ ERROR
+
+        // Access object using method
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+
+        // Check both references
+        if (s1 == s2) {
+            System.out.println("Both references point to same object");
+        }
+    }
+}
