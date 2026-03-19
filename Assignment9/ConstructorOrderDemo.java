98242fdcbea9bcb724f5c78e377fad02897bc79a
swamini <swaminibhandare10@gmail.com>
2026-03-19 11:25:54 +0530

    constructor execution order in inheritance

---
+++ Assignment9/ConstructorOrderDemo.java
@@ -0,0 +1,25 @@
+//Write a program showing constructor execution order in inheritance.
+
+class A {
+    A() {
+        System.out.println("Constructor of class A");
+    }
+}
+
+class B extends A {
+    B() {
+        System.out.println("Constructor of class B");
+    }
+}
+
+class C extends B {
+    C() {
+        System.out.println("Constructor of class C");
+    }
+}
+
+public class ConstructorOrderDemo {
+    public static void main(String[] args) {
+        C obj = new C();
+    }
+}
