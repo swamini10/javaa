1d67856295eaecdfee7d647daf436eeafcd507fb
swamini <swaminibhandare10@gmail.com>
2026-03-21 15:31:29 +0530

    exception if value is negative.  try catch

---
+++ Assignment10/Exception1.java
@@ -0,0 +1,22 @@
+//Create a class where constructor throws an exception if value is negative.
+public class Exception1 {
+    private int value;
+
+    public Exception1(int value) {
+        if (value < 0) {
+            throw new IllegalArgumentException("Value cannot be negative");
+        }
+        this.value = value;
+    }
+    public static void main(String[] args) {
+        try {
+            Exception1 obj1 = new Exception1(10);
+            System.out.println("Object created with value 10");
+
+            Exception1 obj2 = new Exception1(-5); 
+        } catch (IllegalArgumentException e) {
+            System.out.println("Exception caught: " + e.getMessage());
+        }
+        System.out.println("Program continues...");
+    }
+}
