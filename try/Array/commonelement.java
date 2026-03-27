c0406400af0dc7e73416024df9c150467295d7ae
swamini <swaminibhandare10@gmail.com>
2026-03-27 23:58:02 +0530

    find common elements between two arrays

---
+++ try/Array/commonelement.java
@@ -0,0 +1,9 @@
+// Q15. Write a java program to find common elements between two arrays.
+// Input :
+//  Array1 = {1, 2, 3, 4, 5}
+//  Array2 = {3, 4, 5, 6, 7}
+// Output : Common elements = {3, 4, 5}
+// Explanation : Compare each element of Array1 with all elements of Array2, if match found → it is a common element.
+
+public class commonelement{
+}
