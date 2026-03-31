8a7a99c36df7a0797378ffd30d35e39aa2dbdf66
swamini <swaminibhandare10@gmail.com>
2026-03-31 14:22:34 +0530

    common elements between two arrays

--- try/Array/commonelement.java
+++ try/Array/commonelement.java
@@ -6,4 +6,24 @@
 // Explanation : Compare each element of Array1 with all elements of Array2, if match found → it is a common element.
 
 public class commonelement{
+	public static void main(String args[]){
+		 int array1[] ={1, 2, 3, 4, 5};
+		 System.out.print("Array1 :");
+		 for(int j=0;j<array1.length;j++){
+			 System.out.print(array1[j] + " ");
+		 }
+		  int array2[] ={3, 4, 5, 6, 7};
+		 System.out.print("\nArray2 :");
+		 for(int j=0;j<array2.length;j++){
+			 System.out.print(array2[j] + " ");
+		 }
+		 System.out.print("\nCommon elements :");
+		  for(int j=0;j<array1.length;j++){
+			  for(int i=0;i<array2.length;i++){
+				  if(array1[j]==array2[i]){
+					 System.out.print(array1[j] + " "); 
+				  }
+		     }
+		  }
+	}
 }
