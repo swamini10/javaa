ba45e6282650fe505760535c60c6022f34fb6c25
swamini <swaminibhandare10@gmail.com>
2026-04-11 18:27:52 +0530

    creat ArrayList and add element

---
+++ try/Collection/List/ArrayListExample.java
@@ -0,0 +1,25 @@
+
+import java.util.ArrayList;
+public class ArrayListExample{
+	public static void main(String args[]){
+		ArrayList<String>ArrayList = new ArrayList<>();
+		
+		// ArrayList.add("apple");
+		// ArrayList.add("Apple");
+        // ArrayList.add("Banana");
+        // ArrayList.add("Mango");
+
+        // System.out.println(ArrayList);
+		   ArrayList<String> list = new ArrayList<>();
+
+        // Add elements
+        list.add("apple");
+        list.add("Apple");
+        list.add("Banana");
+        list.add("Mango");
+
+        // Print elements
+        System.out.println(list);
+		
+	}
+}
