bfc3147bb79a61c640be09b696f4b9908a8a2822
swamini <swaminibhandare10@gmail.com>
2026-04-14 00:38:40 +0530

    reduce time comp of Prime

--- Test1.java
+++ Test1.java
@@ -15,15 +15,15 @@ public static int fact(int num){
 }
 
 public static boolean Prime(int num){
-	if(num<=1){
-		return false;
-	}
-	for(int i=0;i<=num;i++){
-	if(num % 2==0){
-		return true;
-	}
-}
- return false;
+    if(num <= 1){
+        return false;
+    }
+    for(int i = 2; i <= num/2; i++){
+        if(num % i == 0){
+            return false;
+        }
+    }
+    return true;
 }
 public static void main(String args[]){
 int num=5;
