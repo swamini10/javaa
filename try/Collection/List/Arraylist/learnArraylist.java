9a3d84f80fa0df621ac6c95a9ac28fcd7ee676d9
swamini <swaminibhandare10@gmail.com>
2026-04-14 10:09:22 +0530

    simple array

---
+++ try/Collection/List/Arraylist/learnArraylist.java
@@ -0,0 +1,10 @@
+public class learnArraylist {
+	public static void main (String args[]){
+		String [] stud = new String[30];//array withfixsize
+		stud[0]="Swamini";
+		
+		// now we wont to add new student in it that is 
+		stud [30]="raj"; // it give and exception 
+		// array has fix size 
+	}
+}
