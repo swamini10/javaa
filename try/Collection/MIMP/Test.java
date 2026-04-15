45bd8313c38f0aa9173f6905818f92eafeb7efc1
swamini <swaminibhandare10@gmail.com>
2026-04-15 23:52:07 +0530

    to Maintain order

--- try/Collection/MIMP/Test.java
+++ try/Collection/MIMP/Test.java
@@ -1,23 +1,34 @@
 
-//Remove duplicate elements from a list.
+
 import java.util.List;
 import java.util.Set;
 import java.util.LinkedHashSet;
 import java.util.HashSet;
+import java.util.HashMap;
 import java.util.Arrays;
 import java.util.LinkedList;
 public class Test {
 	public static void main (String args[]){
+		/* 
+		//Remove duplicate elements from a list.
 	List<Integer> list = Arrays.asList(1,2,3,2,4,1);
 	System.out.println(list);
 	// here we only use set bec set not allow duplicate elements
-Set<Integer> set = new HashSet<>(list);
+   Set<Integer> set = new HashSet<>(list);
   System.out.println(set);
 	
 	
 	// to Maintain order 
 	Set<Integer> se = new LinkedHashSet<>(list);
     System.out.println(se);
+	*/
 	
+	//Find Frequency of Elements
+	  int[] arr = {1, 2, 2, 3, 1, 4, 2};
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : arr) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        System.out.println(map);
 	}
 }
