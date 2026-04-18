f398357e7de52364d34a55911cd60c452efd5288
swamini <swaminibhandare10@gmail.com>
2026-04-19 00:18:09 +0530

    print the prime no from array

--- Tasks by sir/arrayprime.java
+++ Tasks by sir/arrayprime.java
@@ -1,20 +1,40 @@
 //find and print the prime no from array
 
 import java.util.Scanner;
-public class arrayprime
-{
-	public static void main(String args [])
-	{
-		Scanner sc = new Scanner(System.in);
-        System.out.print("Enter array size : ");
+
+public class ArrayPrime {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
         int n = sc.nextInt();
+
         int[] arr = new int[n];
-        System.out.print("Enter " + n + " values:");
-        for (int i = 0; i < n; i++) 
-		{
-            arr[i] = sc.nextInt(); 
-           if(arr[i]%2==0)
-			System.out.print(arr[i]+" ");
-		}				
-	}
+
+        System.out.print("Enter " + n + " values: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Prime numbers are:");
+
+        for (int i = 0; i < n; i++) {
+            boolean isPrime = true;
+
+            if (arr[i] <= 1) {
+                isPrime = false;
+            }
+
+            for (int j = 2; j <= arr[i] / 2; j++) {
+                if (arr[i] % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
 }
