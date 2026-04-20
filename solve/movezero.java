22b5abaa3a4729f5d0e20bb52fc092c50591cb74
swamini <swaminibhandare10@gmail.com>
2026-04-20 15:01:43 +0530

    move zero at end

--- solve/movezero.java
+++ solve/movezero.java
@@ -1,5 +1,6 @@
 public class movezero {
-    public static void main(String args[]) {
+    /* 
+   public static void main(String args[]) {
         int arr[] = {2,0,9,0,8,0,7,6,0,5,6,3};
         int num = arr.length;
 
@@ -13,6 +14,22 @@ public class movezero {
             if (arr[i] == 0) {
                 System.out.print(arr[i]);
             }
+        }
+    }*/
+    public static void main(String args[]) {
+        int arr[] = { 2, 0, 9, 0, 8, 0, 7, 6, 0, 5, 6, 3 };
+        int num = arr.length;
+        int count = 0;
+        for (int i = 0; i < num; i++) {
+            if (arr[i] != 0) {
+                System.out.print(arr[i]);
+            } else {
+                count++;
+            }
+        }
+        for (int i = 0; i < count; i++) {
+            System.out.print(0);
+
         }
     }
 }
