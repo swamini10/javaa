9abc4afda9865b28180a9908c91b5a0837f9b5ee
swamini <swaminibhandare10@gmail.com>
2026-04-28 13:43:20 +0530

    array

---
+++ LeetCode/Solution.java
@@ -0,0 +1,53 @@
+/*Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+
+Example 1:
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+Example 2:
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+
+Example 3:
+Input: nums = [3,3], target = 6
+Output: [0,1]
+*/
+import java.util.Scanner;
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i=0;i<nums.length;i++){
+             for (int j=i+1;j<nums.length;j++){
+            if(nums[i]+nums[j]==target){
+        return new int[]{i, j};
+        }
+        }
+    }
+    return new int[]{-1, -1}; // if not found
+    }
+     public static void main(String args[]) {
+               Scanner sc = new Scanner(System.in);
+
+        // Take size of array
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        // Take array elements
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        // Take target
+        System.out.print("Enter target: ");
+        int target = sc.nextInt();
+
+        Solution s = new Solution();
+        int[] result = s.twoSum(nums, target);
+
+    }
+}
